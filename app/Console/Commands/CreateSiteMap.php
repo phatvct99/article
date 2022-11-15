f9fc70f93f7a6fe085c0018e57eec7d57b46715d
phatvct99 <phatvct99@gmail.com>
2022-11-15 22:17:20 +0700

    change sitemap

--- app/Console/Commands/CreateSiteMap.php
+++ app/Console/Commands/CreateSiteMap.php
@@ -91,7 +91,7 @@ class CreateSiteMap extends Command
 
         $business = Business::all();
         foreach ($business as $bus) {
-            if ($counter == 49999) {
+            if ($counter == 30000) {
                 // generate new sitemap file
                 $sitemap->store('xml', 'sitemap-ho-so-cong-ty-' . $sitemapCounter);
                 // add the file to the sitemaps array
