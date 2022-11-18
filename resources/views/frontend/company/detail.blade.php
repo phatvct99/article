f1bea37984c91b4a17574ca7ad15534dcfc96691
phatvct99 <phatvct99@gmail.com>
2022-11-18 22:31:27 +0700

    change template

--- resources/views/frontend/company/detail.blade.php
+++ resources/views/frontend/company/detail.blade.php
@@ -7,14 +7,14 @@
 <section class="bg-body section-space-less30">
     <div class="container">
         <div class="row">
-        
+
             <div class="col-lg-8 col-md-12">
                 <div class="bg-accent p-35-r mb-50 item-shadow-1">
                     <div class="media-body pt-10 media-margin30">
                         <div class="newsletter-area bg-primary">
                             <h2 class="title-medium-light size-xl pl-30 pr-30">Tra cứu thông tin doanh nghiệp trên toàn quốc!</h2>
                             <p>Nhập mã số thuế, tên công ty, người đại diện</p>
-                            <form action="{{ route('search') }}" method="GET" >
+                            <form action="{{ route('search') }}" method="GET">
                                 <div class="input-group stylish-input-group">
                                     <input type="text" name="search" placeholder="Tìm kiếm" class="form-control" value={{old('search')}}>
                                     <span class="input-group-addon">
@@ -24,14 +24,14 @@
                                     </span>
                                 </div>
                             </form>
-                            
+
                         </div>
                     </div>
                 </div>
-            @if(isset($company))
+                @if(isset($company))
                 <h1>Thông tin doanh nghiệp</h1>
                 <div class="bg-accent p-35-r mb-50 item-shadow-1">
-                   <div class="company-detail">
+                    <div class="company-detail">
                         <ul>
                             <li>
                                 <h2>{{$company->name}}</h2>
@@ -54,37 +54,54 @@
                             @endif
                             <br>
                             <li>Ghi chú: Thông tin trên chỉ mang tính tham khảo, có thể doanh nghiệp mới làm thủ tục thay đổi thông tin nên hệ thống chưa cập nhật kịp thời,
-                            Quý khách có thể liên hệ trực tiếp với <strong>{{$company->name}}</strong> tại địa chỉ <a href="https://www.google.com/maps/place/{{$address}}" target="_blank">{{$company->address}}</a> để có thông tin chính xác nhất.
+                                Quý khách có thể liên hệ trực tiếp với <strong>{{$company->name}}</strong> tại địa chỉ <a href="https://www.google.com/maps/place/{{$address}}" target="_blank">{{$company->address}}</a> để có thông tin chính xác nhất.
                             </li>
                         </ul>
-                   </div>
+                    </div>
                 </div>
                 @endif
                 <div class="row">
-                @if((new \Jenssegers\Agent\Agent())->isMobile())
-                <div class="ne-sidebar sidebar-break-md col-lg-4 col-md-12">
-                    <div class="sidebar-box image-ads">
-                        <div class="ne-banner-layout1 text-center">
-                            @if((new \Jenssegers\Agent\Agent())->platform() == 'AndroidOS' )
-                            <a href="https://trackmobi.asia/ZnPC5Qu2" target="_blank">
-                                <img src="/frontend/img/ads/ads4.gif" alt="ad" class="thumbnail-image" loading="lazy">
-                            </a>
-                            @elseif((new \Jenssegers\Agent\Agent())->platform() == 'iOS' )
+                    @if((new \Jenssegers\Agent\Agent())->isDesktop())
+                    <div class="ne-banner-layout1 mb-50 mt-20-r text-center">
+                        @if((new \Jenssegers\Agent\Agent())->platform() == 'AndroidOS' )
+                        <a href="https://trackmobi.asia/ZnPC5Qu2" target="_blank">
+                            <img src="/frontend/img/ads/ads-binance-top.gif" alt="ad" class="thumbnail-image" loading="lazy">
+                        </a>
+                        @elseif((new \Jenssegers\Agent\Agent())->platform() == 'iOS' )
+                        <a href="https://shorten.asia/UBPExYGq" target="_blank">
+                            <img src="/frontend/img/ads/ads-binance-top.gif" alt="ad" class="thumbnail-image" loading="lazy">
+                        </a>
+                        @else
+                        <a href="https://mobilebanking.mbbank.com.vn:8443/referral/referred.html?referral_code=Z61LQN7ZUV171BDYCZPF" target="_blank">
+                            <img src="/frontend/img/ads/ads-binance-top.gif" alt="ad" class="thumbnail-image" loading="lazy">
+                        </a>
+                        @endif
+                    </div>
+                    @endif
+                    @if((new \Jenssegers\Agent\Agent())->isMobile())
+                    <div class="ne-sidebar sidebar-break-md col-lg-4 col-md-12">
+                        <div class="sidebar-box image-ads">
+                            <div class="ne-banner-layout1 text-center">
+                                @if((new \Jenssegers\Agent\Agent())->platform() == 'AndroidOS' )
+                                <a href="https://trackmobi.asia/ZnPC5Qu2" target="_blank">
+                                    <img src="/frontend/img/ads/ads4.gif" alt="ad" class="thumbnail-image" loading="lazy">
+                                </a>
+                                @elseif((new \Jenssegers\Agent\Agent())->platform() == 'iOS' )
                                 <a href="https://shorten.asia/UBPExYGq" target="_blank">
                                     <img src="/frontend/img/ads/ads4.gif" alt="ad" class="thumbnail-image" loading="lazy">
                                 </a>
-                            @else
+                                @else
                                 <a href="https://mobilebanking.mbbank.com.vn:8443/referral/referred.html?referral_code=Z61LQN7ZUV171BDYCZPF" target="_blank">
                                     <img src="/frontend/img/ads/ads4.gif" alt="ad" class="thumbnail-image" loading="lazy">
                                 </a>
-                            @endif
+                                @endif
+                            </div>
                         </div>
                     </div>
-                </div>
-                <br>
-                @endif
-                @if(isset($companyRelate))
-                @foreach($companyRelate as $com)
+                    <br>
+                    @endif
+                    @if(isset($companyRelate))
+                    @foreach($companyRelate as $com)
                     <div class="col-sm-12 col-12">
                         <div class="content-business">
                             <div class="company">
@@ -98,14 +115,14 @@
                                     @endif
                                     <span>Mã số thuế: {{$com->tax}}</span>
                                     <br>
-                                    <em>Địa chỉ:</em> 
+                                    <em>Địa chỉ:</em>
                                     {{$com->address}}
                                 </div>
                             </div>
                         </div>
                     </div>
-                @endforeach
-                @endif
+                    @endforeach
+                    @endif
                 </div>
             </div>
 
@@ -127,13 +144,13 @@
                 <img src="/frontend/img/ads/ads1.gif" alt="ad" class="thumbnail-image" loading="lazy">
             </a>
             @elseif((new \Jenssegers\Agent\Agent())->platform() == 'iOS' )
-                <a href="https://shorten.asia/UBPExYGq" target="_blank">
-                    <img src="/frontend/img/ads/ads1.gif" alt="ad" class="thumbnail-image" loading="lazy">
-                </a>
+            <a href="https://shorten.asia/UBPExYGq" target="_blank">
+                <img src="/frontend/img/ads/ads1.gif" alt="ad" class="thumbnail-image" loading="lazy">
+            </a>
             @else
-                <a href="https://mobilebanking.mbbank.com.vn:8443/referral/referred.html?referral_code=Z61LQN7ZUV171BDYCZPF" target="_blank">
-                    <img src="/frontend/img/ads/ads1.gif" alt="ad" class="thumbnail-image" loading="lazy">
-                </a>
+            <a href="https://mobilebanking.mbbank.com.vn:8443/referral/referred.html?referral_code=Z61LQN7ZUV171BDYCZPF" target="_blank">
+                <img src="/frontend/img/ads/ads1.gif" alt="ad" class="thumbnail-image" loading="lazy">
+            </a>
             @endif
         </div>
     </div>
@@ -144,26 +161,29 @@
 @section('js')
 
 <script>
-window.onscroll = function() {myFunction()};
-function myFunction() {
-    if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        var footerElement = document.getElementById("footerPost");
-        var footerPosition = footerElement.offsetTop;
-        if (document.documentElement.scrollTop > 300 && document.documentElement.scrollTop < (footerPosition- 900) ) {
-        document.querySelector(".image-ads").style.position = "fixed";
-        } else {
-        document.querySelector(".image-ads").style.position = "";
+    window.onscroll = function() {
+        myFunction()
+    };
+
+    function myFunction() {
+        if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            var footerElement = document.getElementById("footerPost");
+            var footerPosition = footerElement.offsetTop;
+            if (document.documentElement.scrollTop > 300 && document.documentElement.scrollTop < (footerPosition - 900)) {
+                document.querySelector(".image-ads").style.position = "fixed";
+            } else {
+                document.querySelector(".image-ads").style.position = "";
+            }
+        }
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                document.getElementById("banner-ads-bottom").style.top = "60px";
+                document.getElementById("banner-ads-bottom").style.position = "fixed";
+            } else {
+                document.getElementById("banner-ads-bottom").style.top = "-60px";
+            }
         }
     }
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-            document.getElementById("banner-ads-bottom").style.top = "60px";
-            document.getElementById("banner-ads-bottom").style.position = "fixed";
-        } else {
-            document.getElementById("banner-ads-bottom").style.top = "-60px";
-        }
-    }
-}
 </script>
 
 @endsection
