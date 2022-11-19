0420d208e506f91500caa47f55962fc4ac0c0815
phatvct99 <phatvct99@gmail.com>
2022-11-19 22:37:22 +0700

    final sitemap

--- app/Console/Commands/CreateSiteMap.php
+++ app/Console/Commands/CreateSiteMap.php
@@ -91,7 +91,7 @@ class CreateSiteMap extends Command
 
         $business = Business::all();
         foreach ($business as $bus) {
-            if ($counter == 49000) {
+            if ($counter == 2000) {
                 // generate new sitemap file
                 $sitemap->store('xml', 'sitemap-ho-so-cong-ty-' . $sitemapCounter);
                 // add the file to the sitemaps array
